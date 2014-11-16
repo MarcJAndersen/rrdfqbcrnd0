fa7fb1652dc291d39aa36d330b99e57eff77b264
MarcJAndersen <mja@statgroup.dk>
2014-11-16 21:20:38 +0100

    removing function qb.addprefix, as the functions uses assign, which is not recommended MJA 2014-11-16

--- R/BuildCube-functions.R
+++ R/BuildCube-functions.R
@@ -254,17 +254,18 @@ buildCodelist <- function(store,prefixlist,obsData,codeType,nciDomainValue,dimNa
   }
 } # end buildCodelist()
 
-qb.addprefix<- function(prefixSource) {
-for (i in 1:nrow(prefixSource))
-{
-  tempName <-paste0("prefix",toupper(prefixSource[i,"prefix"]))
-  assign(tempName,as.character(prefixSource[i,"namespace"]), envir=globalenv()) # MJA 2014-10-13
-  # Use as.character to get the enquoting needed by the function
-  add.prefix(store,as.character(prefixSource[i,"prefix"]),
-             as.character(prefixSource[i,"namespace"]))
-}
-invisible(TRUE)
-}
+## removing function qb.addprefix, as the functions uses assign, which is not recommended MJA 2014-11-16
+## qb.addprefix<- function(prefixSource) {
+## for (i in 1:nrow(prefixSource))
+## {
+##   tempName <-paste0("prefix",toupper(prefixSource[i,"prefix"]))
+##   assign(tempName,as.character(prefixSource[i,"namespace"]), envir=globalenv()) # MJA 2014-10-13
+##   # Use as.character to get the enquoting needed by the function
+##   add.prefix(store,as.character(prefixSource[i,"prefix"]),
+##              as.character(prefixSource[i,"namespace"]))
+## }
+## invisible(TRUE)
+## }
 
 qb.def.prefixlist<- function(store, prefixSource) {
 pl<- list();
