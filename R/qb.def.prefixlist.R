54d7a5a569e21b339a8238b8ac3ed8941d5a5147
MarcJAndersen <mja@statgroup.dk>
2014-12-26 20:21:10 +0100

    Added possibility of model=NULL

--- R/qb.def.prefixlist.R
+++ R/qb.def.prefixlist.R
@@ -1,10 +1,10 @@
 ##' Add prefixes to a rrdf store
 ##'
 ##' Sideeffect: the prefixes are added to the store
-##' @param store A rrdf store
+##' @param store A rrdf store, if NULL then not added to a store
 ##' @param prefixes A data.frame with column prefix and namespace
 ##' @return The list with member names prefixUPPERCASEPREFIX and namespace as value
-qb.def.prefixlist<- function(store, prefixes) {
+qb.def.prefixlist<- function(store=NULL, prefixes) {
 pl<- list();
 
 for (i in 1:nrow(prefixes))
@@ -14,8 +14,10 @@ for (i in 1:nrow(prefixes))
       as.character(prefixes[i,"namespace"])
 
   # Use as.character to get the enquoting needed by the function
+  if (!is.null(store)) {
   add.prefix(store, as.character(prefixes[i,"prefix"]),
                     as.character(prefixes[i,"namespace"]))
+}
 }
 return(pl)
 }
