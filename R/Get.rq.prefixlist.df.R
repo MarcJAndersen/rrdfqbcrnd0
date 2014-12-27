90dbd575c4b4b932243b287d4db26e8194203c2e
MarcJAndersen <mja@statgroup.dk>
2014-12-27 23:39:11 +0100

    New function

---
+++ R/Get.rq.prefixlist.df.R
@@ -0,0 +1,19 @@
+##' Create prefix definition for SPARQL query from a prefix list
+##'
+##' The function defines the domain specifif prefixes used for the rdf
+##' data cube for the Clinical Research and Development Analysis
+##' Results Data
+##' 
+##' @param prefixlist List of prefixes made by qb.def.prefixlist
+##' @return String containing prefix statements for SPARQL query
+Get.rq.prefixlist.df<- function( prefixlist ) {
+
+forsparqlprefix<- paste0(paste(
+  "prefix",
+  paste(tolower(gsub("^prefix", "", names(prefixlist))),":",sep=""),
+  paste("<",prefixlist,">",sep=""),
+  sep=" ",collapse="\n"),"\n")
+
+forsparqlprefix
+}
+  
