2bd4be776c468d19850d8e7b887b3143cc262b1c
MarcJAndersen <mja@statgroup.dk>
2014-12-27 23:37:06 +0100

    Added function for defining SPARQL prefix part from data.frame

--- R/Get.rq.prefix.df.R
+++ R/Get.rq.prefix.df.R
@@ -4,16 +4,15 @@
 ##' data cube for the Clinical Research and Development Analysis
 ##' Results Data
 ##' 
-##' @param domainName The domainName to create prefixes for
-##' @param namespaceStem The stem for the namespace
-##' @return Data frame with column prefix and namespace
+##' @param prefix.df prefixes as data.frame with column prefix and namespace
+##' @return String containing prefix statements for SPARQL query
 Get.rq.prefix.df<- function( prefix.df ) {
 
-forsparqlprefix<- paste(
+forsparqlprefix<- paste0(paste(
   "prefix",
   paste(prefix.df$prefix,":",sep=""),
   paste("<",prefix.df$namespace,">",sep=""),
-  sep=" ",collapse="\n")
+  sep=" ",collapse="\n"),"\n")
 
 forsparqlprefix
 }
