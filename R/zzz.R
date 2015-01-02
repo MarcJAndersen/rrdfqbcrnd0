1d3f11e1977cd4984f87e83f79a88c674337d5d1
MarcJAndersen <mja@statgroup.dk>
2015-01-02 21:26:11 +0100

    Data qbCDISCprefixes is now created in local environment instead of as a data

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,31 @@
 ##' 
 ##'
-##' Start-up code
-##' @param libname 
-##' @param pkgname 
+##' Start-up code for defining cdiscstandards and qbCDISCprefixes
+##' @param libname library name
+##' @param pkgname package name
 ##' @return invisible
 
 env <- new.env(parent=emptyenv()) 
 
 .onLoad <- function(libname, pkgname) {
   env[["cdiscstandards"]]<- NULL
+
+  env[["qbCDISCprefixes"]]<-  list(
+ "rdf"= "http://www.w3.org/1999/02/22-rdf-syntax-ns#" ,
+ "skos"="http://www.w3.org/2004/02/skos/core#" ,
+ "prov"="http://www.w3.org/ns/prov#" ,
+ "rdfs"="http://www.w3.org/2000/01/rdf-schema#" ,
+ "dcat"="http://www.w3.org/ns/dcat#" ,
+ "owl"= "http://www.w3.org/2002/07/owl#" ,
+ "xsd"= "http://www.w3.org/2001/XMLSchema#" ,
+ "qb"=  "http://purl.org/linked-data/cube#" ,
+ "pav"= "http://purl.org/pav" ,
+ "dct"= "http://purl.org/dc/terms/" ,
+ "mms"= "http://rdf.cdisc.org/mms#" ,
+ "cts"= "http://rdf.cdisc.org/ct/schema#",
+ "rrdfqbcrnd0"="http://www.example.org/rrdfqbcrnd0/"
+)  ;
+
   invisible()
 }
 
