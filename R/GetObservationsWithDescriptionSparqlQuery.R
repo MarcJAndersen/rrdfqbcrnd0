4a95319d4cd54d449bb330032d05007b619db72f
MarcJAndersen <mja@statgroup.dk>
2015-01-11 22:33:00 +0100

    SPARQL query providing, observations, label and IRIs

---
+++ R/GetObservationsWithDescriptionSparqlQuery.R
@@ -0,0 +1,39 @@
+##' SPARQL query for observations from RDF data cube in workbook format
+##' @param forsparqlprefix PREFIX part of SPARQL query
+##' @param domainName domainName for the RRDF cube
+##' @param dimensions dimensions
+##' @param attributes attributes 
+##' @return SPARQL query
+GetObservationsWithDescriptionSparqlQuery<- function( forsparqlprefix, domainName, dimensions, attributes ) {
+cube.observations.rq<-  paste( forsparqlprefix,
+    "select * where {",
+    "?s a qb:Observation  ;", "\n",
+    paste("       qb:dataSet",  paste0( "ds:", "dataset", "-", domainName), " ;", sep=" ", collapse="\n"), "\n",
+    paste0( dimensions, " ", sub("prop:", "?", dimensions), ";", collapse="\n"),
+    paste0( attributes, " ", sub("prop:", "?", attributes), ";", collapse="\n"),
+    "prop:measure      ?measure ;      \n",
+    paste0( "optional{ ", sub("prop:", "?", dimensions), " ",
+           "skos:prefLabel",
+           " ",
+           sub("prop:", "?", dimensions), "value" ,
+           " . ", "}",
+           collapse="\n"),
+    paste0( "optional{ ", dimensions, " ",
+           "rdfs:label",
+           " ",
+           sub("prop:", "?", dimensions), "label" ,
+           " . ", "}",
+           collapse="\n"),
+    paste0( "BIND( IRI(", dimensions, ")",
+           "as",
+           " ",
+           sub("prop:", "?", dimensions), "IRI" ,
+            ")",
+           collapse="\n"),
+    "BIND( IRI( ?s ) AS ?measureIRI)",                              
+    "\n",
+    "} ",
+    "\n"
+   )
+cube.observations.rq
+}
