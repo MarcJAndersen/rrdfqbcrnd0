05d282ffe361bdc862e9ceca099473c7efbb01bd
MarcJAndersen <mja@statgroup.dk>
2015-04-03 11:26:22 +0200

    Added description of status

--- rrdfqbcrnd0/vignettes/write-rdf-in-other-formats.R
+++ rrdfqbcrnd0/vignettes/write-rdf-in-other-formats.R
@@ -4,12 +4,16 @@ store<- new.rdf()
 dataCubeFile<- system.file("extdata/sample-rdf", "DC-DEMO-sample.TTL", package="rrdfqbcrnd0")
 print(dataCubeFile)
 load.rdf(dataCubeFile, format="TURTLE", appendTo= store)
+summarize.rdf(store)
 
 format<-"RDF/JSON"
 filename<-"DC-demo.json"
 .jcall("com/github/egonw/rrdf/RJenaHelper", "V", "saveRdf", store, filename, format)
 
-# https://jena.apache.org/documentation/io/rdf-output.html
+## This does not work - maybe due mising jar in rrdf
+## Error in .jcall("com/github/egonw/rrdf/RJenaHelper", "V", "saveRdf", store,  :
+##  java.lang.NoClassDefFoundError: com/github/jsonldjava/core/JsonLdError
+## ttps://jena.apache.org/documentation/io/rdf-output.html
 format<-"JSON-LD"
 filename<-"DC-demo.json-ld"
 .jcall("com/github/egonw/rrdf/RJenaHelper", "V", "saveRdf", store, filename, format)
