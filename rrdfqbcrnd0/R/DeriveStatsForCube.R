95ecc56ef14da49a17e01c5d3b110fdc6e569816
MarcJAndersen <mja@statgroup.dk>
2015-04-06 22:28:31 +0200

    Derivations of statistics works

--- rrdfqbcrnd0/R/DeriveStatsForCube.R
+++ rrdfqbcrnd0/R/DeriveStatsForCube.R
@@ -2,40 +2,57 @@
 ##' 
 ##' @inheritParams GetObservationsSparqlQuery
 ##' @param store a RRDF store containing one or more date cubes
+##' @param forsparqlprefix prefixes for SPARQL queries
 ##' @param domainName domainName for the RRDF cube
-##' @param dsdName Dataset Descriptor Name
 ##' @param dataSet data.frame containing dataset to validate against/derive measures
+##' @param deriveMeasureList TODO(mja): add code for this variabel
 ##' @param validation.measure list with property, prefix and
 ##' URIprefix. The derived results is stored in the cube in the store
 ##' using the property. This is only used is checkOnly==TRUE
-##' @param checkOnly: TRUE if only the check is performed (default), FALSE then measure is overwritten with the results
+##' @param dsdName Dataset Descriptor Name
+##' @param checkOnly TRUE if only the check is performed (default),
+##' FALSE then measure is overwritten with the results
+##' @param myprefixes Prefixes used for storing the results - TODO(mja): combined with forsparqlprefix
 ##' @return list with status TRUE if the operation was sucessfull
-##' 
-DeriveStatsForCube<- function(store, forsparqlprefix, domainName, dsdname, dataSet, deriveMeasureList=NULL, checkOnly=TRUE, validation.measure=NULL ) {
+DeriveStatsForCube<- function(store, forsparqlprefix, domainName, dsdName, dataSet, deriveMeasureList=NULL, checkOnly=TRUE, validation.measure=NULL, myprefixes ) {
 
+  if (!is.data.frame(dataSet)) {
+    message("Changing dataSet to data.frame")
+    dataSet<- as.data.frame(dataSet)
+  }
   ## obsFile=paste(tempdir(),"/", "adsl", ".xpt",sep="")
   ## domainName="demog"
   
-  if (is.null(validation.measure) & checkOnly==TRUE ) {
+  if (is.null(validation.measure)  ) {
     validation.measure<- NULL
     validation.measure$property<- "qbderiv:result"
     validation.measure$prefix<- unlist(strsplit(validation.measure$property,":"))[1]
-    validation.measure$namespace<- paste0("http://www.example.org/dc/",domainName,"/validmeas/")
+    validation.measure$namespace<- paste0("http://www.example.org/dc/",tolower(domainName),"/validmeas/")
   }
 
   codelists.rq<- GetCodeListSparqlQuery( forsparqlprefix, dsdName )
+##  cat(codelists.rq,"\n")
   cube.codelists<- as.data.frame(sparql.rdf(store, codelists.rq), stringsAsFactors=FALSE)
+##  str(cube.codelists)
 
+  Ndiff<-0
+  
+##  print(cube.codelists$prefLabel)
   codelist.all<- cube.codelists[ cube.codelists$prefLabel=="_ALL_",]
-  ## print(codelist.all)
+##  print(codelist.all)
   subsetting.dimensions<- list();
 
   ## TODO(mja): the variable name/column name in the data frame should be part of the datacube.
   ## This would remove the need for the workaround below using gsub
   for (i in 1:nrow(codelist.all))
     {
-      subsetting.dimensions[[ gsub("[^:]*:","", codelist.all[i,"p"]) ]] <-
+      codeName<- gsub("[^:]*:","", codelist.all[i,"p"])
+      if (! (codeName %in% c("factor", "procedure")) ) {
+        ## TODO(mja): when denominator also becomes a dimension, then include in list
+##        cat("codeName: ", codeName, "\n" )
+      subsetting.dimensions[[codeName]] <-
         as.character(codelist.all[i,"cl"])
+    }
     }
 
   dimensionsRq <- GetDimensionsSparqlQuery( forsparqlprefix )
@@ -57,122 +74,157 @@ DeriveStatsForCube<- function(store, forsparqlprefix, domainName, dsdname, dataS
   ## TODO: this should be defined in a RDF model
   ## using list for key-value lookup to function for descriptive statistic
   univfunc1= list(
-    "code:procedure-MEAN"=mean,
-    "code:procedure-STDDEV"=sd,
-    "code:procedure-MEDIAN"=median,
-    "code:procedure-MIN"=min,
-    "code:procedure-MAX"=max
+    "code:procedure-mean"=function(x){mean(x, na.rm=TRUE)},
+    "code:procedure-stddev"=function(x){sd(x, na.rm=TRUE)},
+    "code:procedure-stdev"=function(x){sd(x, na.rm=TRUE)},
+    "code:procedure-median"=function(x){median(x, na.rm=TRUE)},
+    "code:procedure-min"=function(x){min(x, na.rm=TRUE)},
+    "code:procedure-max"=function(x){max(x, na.rm=TRUE)}
     )
 
   univfunc2= list(
-    "code:procedure-COUNT"=length,
-    "code:procedure-COUNTDISTINCT"=function(x){length(unique(x))}
+    "code:procedure-count"=length,
+    "code:procedure-countdistinct"=function(x){length(unique(x))}
     )
 
 if (nrow(observations )<1) {
   stop("no rows in observations")
 }
-## print(nrow(observations ))
 
+##  cat("Subsetting dimensions: ", paste0(names(subsetting.dimensions),sep=", "),"\n")
+  
   for (r in  1:nrow(observations )  ) {
-    thisrow<-  observations[r,]
-## print(thisrow)
+##    cat("Cube observation sequence number ", r,  ".\n" )
+    thisobs<-  observations[r,]
+##    cat("   Derive results for procedure ", as.character(thisobs["procedure"]),
+##        "factor", as.character(thisobs["factor"]), "\n")
 
     ## TODO(mja): handle _NONMISS_, see GetSQLFromCube.R for approach
     data.subset.logical<- rep(TRUE, nrow(dataSet))
+    logicalExpr<- ""
     for (v in names(subsetting.dimensions)) {
-##       print( c(v, thisrow[v ],  subsetting.dimensions[[ v ]] ) )
-      if ( thisrow[v ] != subsetting.dimensions[[ v ]] ) {
-##        print( paste0(v, ": ", thisrow[v ], " = ", thisrow[ paste0(v,"value") ]))
-        ## print( dataSet[,toupper(v)]  )
-        ## print(  thisrow[ paste0(v,"value") ] )
-        ## print( dataSet[,toupper(v)] == as.character(thisrow[ paste0(v,"value") ]) )
-        data.subset.logical<- data.subset.logical & ( dataSet[,toupper(v)] == as.character(thisrow[ paste0(v,"value") ]) )
+      if ( thisobs[v ] != subsetting.dimensions[[ v ]] ) {
+        logicalExpr<- paste0( logicalExpr, paste0(toupper(v), "==", as.character(thisobs[ paste0(v,"value") ])), sept=" & " )
+        data.subset.logical<- data.subset.logical & ( dataSet[,toupper(v)] == as.character(thisobs[ paste0(v,"value") ]) )
       }
     }
+
+##    cat("   ", "Data subsetting expression ", logicalExpr, ".\n",
+##        "   Data set row contributing ", sum(data.subset.logical), "\n")
     
     has.result<- FALSE
     
-    if (thisrow["procedure"] %in% names(univfunc1) ) {
-    ## print("univfunc1")
-      AOD<- dataSet[data.subset.logical,thisrow["factorvalue"]]
-      result<- univfunc1[[ thisrow["procedure"] ]](AOD)
+    if (thisobs["procedure"] %in% names(univfunc1) ) {
+      AOD<- as.vector(dataSet[data.subset.logical, as.character(thisobs["factorvalue"])])
+      result<- univfunc1[[ as.character(thisobs["procedure"]) ]](AOD)
       has.result<- TRUE
       ##   print(paste("AOD number of observations", nrow(AOD)))
-    } else if (thisrow["procedure"] %in% names(univfunc2) ) {
-      ## print(paste0("univfunc2"," ",thisrow["procedure" ],collapse=" "))
-      AOD<- dataSet[data.subset.logical,"USUBJID"]  # take the first variable, would be better if there was a default, like USUBJID
-      result<- univfunc2[[ thisrow["procedure" ] ]](AOD)
+    } else if (thisobs["procedure"] %in% names(univfunc2) ) {
+##      cat("  ", paste0("univfunc2"," ",thisobs["procedure" ],collapse=" "), "\n")
+      ## Here the result must be a vector
+      AOD<- dataSet$USUBJID[data.subset.logical]
+      result<- univfunc2[[ as.character(thisobs["procedure" ]) ]](AOD)
       has.result<- TRUE
-      ##   print(paste0("AOD number of observations", nrow(AOD), sep=" "))
-    } else if (thisrow["procedure"]== "code:procedure-PERCENT" & thisrow["factor"]== "code:factor-PROPORTION") {
+##      cat(paste0("AOD number of observations", length(AOD), sep=" "),"\n")
+    } else if (thisobs["procedure"]== "code:procedure-percent" & thisobs["factor"]== "code:factor-proportion") {
       
-      ##  print(c("proportion",thisrow["denominator"]))
-      denom.def<- thisrow[ names(subsetting.dimensions) ]
-      denom.def[ tolower(thisrow["denominator"]) ] =paste0("code:",tolower(thisrow["denominator"]),"-_ALL_",sep="")
-      AOD<- dataSet[data.subset.logical,] # should use a variable name - USUBJID like for count
+      denom.def<- tolower(thisobs["denominator"])
+##      cat( "   ", paste0( "Denominator definition: ", denom.def ), "\n")
 
       denom.data.subset.logical<- rep(TRUE, nrow(dataSet))
-      ## print( denom.def )
-      for (v in names(denom.def)) {
-        if ( denom.def[v ] != subsetting.dimensions[[ v ]] ) {
-          denom.data.subset.logical<- denom.data.subset.logical & ( dataSet[,toupper(v)] == thisrow[ paste0(v,"value") ] )
-        }
+      for (v in setdiff(names(subsetting.dimensions),denom.def) ) {
+        if ( thisobs[v ] != subsetting.dimensions[[ v ]] ) {
+        subset.for.var<-dataSet[,toupper(v)] == as.character(thisobs[ paste0(v,"value") ])
+        denom.data.subset.logical<- denom.data.subset.logical & subset.for.var
       }
-      denom.data.frame<-  dataSet[denom.data.subset.logical , ]
+      }
+
+      AOD<- dataSet$USUBJID[data.subset.logical]
+      denom.data<- dataSet$USUBJID[denom.data.subset.logical]
       
-      result<- nrow(AOD) / nrow( denom.data.frame ) * 100;
+##      cat("   ", paste("AOD number of observations", length(AOD)),"\n")
+##      cat("   ", paste("denom.data number of observations", length(denom.data)),"\n")
+
+      result<- length(AOD) / length( denom.data ) * 100;
       has.result<- TRUE
-      ## print(paste("AOD number of observations", nrow(AOD)))
-      ## print(paste("denom.data.frame number of observations", nrow(denom.data.frame)))
     }
 
 
 
     if (has.result) {
-      ## print(paste("result", result, sep=" "))
+##      cat("   ", paste("result", result, " in cube ", thisobs["measure"], sep=" "), "\n" )
+      if (result != thisobs["measure"]) {
+        Ndiff<- Ndiff+1
+      }
+      if (! checkOnly ) {
+      subj<- gsub("ds:", myprefixes$prefixDS, thisobs["s"])
+      pred<- gsub(paste0(validation.measure$prefix,":"),validation.measure$namespace,validation.measure$property)
+##      cat( "-->  ", subj, "\n")
+##      cat( "-->  ", pred, "\n")
+##      cat( "-->  ", paste(result), "\n")
       add.data.triple( store,
-                      subject=gsub("ds:", myprefixes$prefixDS, thisrow["s"]), # add triple does not resolve prefix
-                      predicate=gsub(validation.measure$prefix,validation.measure$namespace,validation.measure$property),
+                      subject=subj, 
+                      predicate=pred,
                       data=paste(result), type="float")
-    } else {
-      ##  print( paste( thisrow["s"], ifelse(has.result, result, "No result determined") ) )
+    }
+    }
+    else {
+      Ndiff<- Ndiff+1
+##      print( paste( thisobs["s"], ifelse(has.result, result, "No result determined") ) )
     }
     
     
   } # for
-  
-prval<- paste0("prefix ", validation.measure$prefix,": ","<", validation.measure$namespace, ">\n")
-print(prval)
-cube.measure.result.rq<- paste(forsparqlprefix, prval,
-      "select * where {",
-      "    ?s a qb:Observation  ;",
-      paste("       qb:dataSet",  paste0("ds:",dsdName), " ;", sep=" ", collapse=" "),
-      "       crnd:measure      ?measure ;      ",
-      "      optional{ ?s ", validation.measure$property, " ?result }      ",
-      "} order by ?s"
-)
 
-cat(cube.measure.result.rq)
+  cat("Number of differences ", Ndiff, "\n")
+  
+if (! checkOnly) {  
+prval<- paste0("prefix ", validation.measure$prefix,": ","<", validation.measure$namespace, ">\n")
+
+  ## print(prval)
+  cube.measure.result.rq<-
+    paste(forsparqlprefix, prval,
+          "select * where {",
+          "    ?s a qb:Observation  ;",
+          paste0("       qb:dataSet", " ", paste0( "ds:", "dataset", "-", domainName), " ;", sep=" ", collapse=" "),
+          "       crnd-dimension:procedure      ?procedure ;      ",
+          "       crnd-measure:measure      ?measure ;      ",
+          "    .",
+          paste0("      optional{ ?s ", validation.measure$property, " ?result }      "),
+          "} order by ?s",
+          "\n",
+          sep="\n"
+          )
+
+## cat(cube.measure.result.rq, "\n" )
 cube.measure.result<-  sparql.rdf(store, cube.measure.result.rq);
   
-  ## print(cube.measure.result)
+## print(cube.measure.result)
   
-  
-  cube.check<- sparql.rdf(store,
-                          paste(forsparqlprefix, prval,
-                                "select * where {",
-                              "    ?s a qb:Observation  ;",
-                                paste("       qb:dataSet",  paste0("ds:",dsdName), " ;", sep=" ", collapse=" "),
-                                "       crnd:measure      ?measure ;      ",
-                                "      optional{ ?s ", validation.measure$property, " ?result }      ",
-                                " filter ( ?measure != ?result ) ",
-                                "} order by ?s"
-                                )
-                          );
+  filterexpr<- " filter ( (xsd:float(?measure)-xsd:float(?result)) != 0 || !bound(?result) ) "
+#  filterexpr<- " "
+
+cube.check.rq<-  
+    paste(forsparqlprefix, prval,
+          "select ?s ?procedure ?measure ?result ((xsd:float(?measure)-xsd:float(?result)) as ?diff) where {",
+          "    ?s a qb:Observation  ;",
+          paste0("       qb:dataSet", " ", paste0( "ds:", "dataset", "-", domainName), " ;", sep=" ", collapse=" "),
+          "       crnd-dimension:procedure      ?procedure ;      ",
+          "       crnd-measure:measure      ?measure ;      ",
+          "    .",
+          paste0("      optional{ ?s ", validation.measure$property, " ?result }      "),
+          filterexpr,
+          "} order by ?s",
+          "\n",
+          sep="\n"
+          )
+
+  cube.check<- sparql.rdf(store, cube.check.rq );
   
   print("If the result is <0 x 0> matrix then all value matches")
-  cube.check
-  
-  
+
+  return(cube.check)
+} else {
+  return(Ndiff==0)
+}
 }
