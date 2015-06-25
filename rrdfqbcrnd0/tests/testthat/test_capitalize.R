134d58d125eb4bc6052c696ed523243f0b29fdac
Ippei <ippei@ip-172-31-37-225.us-west-2.compute.internal>
2015-06-25 15:03:05 +0000

    First version test code for capitalize function

--- rrdfqbcrnd0/tests/testthat/test_capitalize.R
+++ rrdfqbcrnd0/tests/testthat/test_capitalize.R
@@ -4,3 +4,9 @@
 library(testthat)
 
 ## Write test code below
+
+test_that("check the values in DEMO-Components", {
+  expect_equal(capitalize("string and text", strict = TRUE), "STRING AND TEXT")
+  expect_equal(capitalize("string and text", strict = FALSE), "String And Text")
+})
+
