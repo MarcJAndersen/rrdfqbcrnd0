ea629b607cbf478e1a630cf594e7649c039e611d
Marc Andersen <mja@statgroup.dk>
2015-12-30 20:12:27 +0100

    Added function name to @export due roxygen2 handling of names with .

--- rrdfancillary/R/update.rdf.R
+++ rrdfancillary/R/update.rdf.R
@@ -1,14 +1,17 @@
 ##' SPARQL update
+##' 
 ##' @param sparqlUpdateStmts SPARQL update statements string
 ##' @param model RRDF store
 ##' @return TRUE
-##' @export
+##' @export update.rdf
+##' @usage  update.rdf(model, sparqlUpdateStmts)
 ##'
 ## Same parameters as construct
 ## ~/packages/rrdf/rrdf/R/construct.R
 ## ~/packages/rrdf/rrdf/R/save.rdf.R
 ## Corresponding java code for RJenaHelper2
 ## ../java/src/com/github/marcjandersen/rrdfancillary/RJenaHelper2.java
+
 update.rdf <- function(model, sparqlUpdateStmts) {
     .jcall(
         "com/github/marcjandersen/rrdfancillary/RJenaHelper2",
--- rrdfancillary/java/src/com/github/marcjandersen/rrdfancillary/RJenaHelper2.java
+++ rrdfancillary/java/src/com/github/marcjandersen/rrdfancillary/RJenaHelper2.java
@@ -1,53 +1,7 @@
 
 package com.github.marcjandersen.rrdfancillary;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpException;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.StatusLine;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
-import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
-import com.hp.hpl.jena.query.Query;
-import com.hp.hpl.jena.query.QueryExecution;
-import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.query.QueryFactory;
-import com.hp.hpl.jena.query.QuerySolution;
-import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.ResultSetFactory;
-import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.rdf.model.RDFNode;
-import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.rdf.model.SimpleSelector;
-import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.rdf.model.StmtIterator;
-import com.hp.hpl.jena.shared.PrefixMapping;
-import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP;
 
 // from apache-jena-2.13.0/src-examples/arq/examples/update/UpdateExecuteOperations.java
 // import org.apache.jena.atlas.lib.StrUtils ;
